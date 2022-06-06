c38014615de4bcdba75baf4e495afc58f4a5ad7b
Leonardo <leocastabelli@gmail.com>
2022-06-06 20:43:43 -0300

    LineUp changes
    - Add Trophy Room page
    - Add Index Page
    - LineUp Players Changes

--- soccerField.js
+++ soccerField.js
@@ -1,11 +1,18 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+const players = {
+
+  GK: {
+    name: "Severus",
+    number: 45,
+    profilePhoto: "img/players/GK.png"
+  },
+
+  LB: {
+    name: "Gomez",
+    number: 22,
+    profilePhoto: "img/players/LB.png"
   }
+
   
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
+
+
+}
